fe11ee3962a7dc402de7945fa91134aa5f8e6b06
maojj <maojj@fenbi.com>
2016-08-12 19:18:48 +0800

    MOD: draw multi path for a line

--- newDraw/HandDrawView.swift
+++ newDraw/HandDrawView.swift
@@ -49,20 +49,26 @@ class HandDrawView: UIView {
     private var currentTouchTime: NSTimeInterval = 0
     private var preIndex: Int = 0
 
+
+    private var lastLeftDrawnPoint: CGPoint = .zero
+    private var lastRightDrawnPoint: CGPoint = .zero
+
+    private var fillPaths: [UIBezierPath] = []
+
+
     override func drawRect(rect: CGRect) {
         let context = UIGraphicsGetCurrentContext()
         CGContextSaveGState(context)
         strokeColor.setFill()
-        if let path = getTempPath() {
-            fillPath = path
+        for path in fillPaths {
+            path.fill()
         }
-        fillPath.fill()
 
         CGContextRestoreGState(context)
     }
 
     func clear() {
-        fillPath.removeAllPoints()
+        fillPaths.removeAll()
         strokePoints.removeAll()
         leftPoints.removeAll()
         rightPoints.removeAll()
@@ -229,12 +235,6 @@ class HandDrawView: UIView {
         return width
     }
 
-    private func getTempPath() -> UIBezierPath? {
-        guard strokePoints.count > 1 else { return nil }
-        let path = StrokeUtils.fillPathForLeftPoints(leftPoints, rightPoints: rightPoints)
-        return path
-    }
-
     private func generateSidePointsForLastPoint(isStrokeFinished: Bool)  {
         guard strokePoints.count >= 2 else { return }
 
@@ -246,24 +246,65 @@ class HandDrawView: UIView {
             let firstPoint = strokePoints[0].point
             let secondPoint = strokePoints[1].point
             let width = strokePoints[0].width * lineWidth
-            addSidePoints(forPoint: firstPoint, startPoint: firstPoint, endPoint: secondPoint, width: width)
+            addSidePoints(forPoint: firstPoint, startPoint: firstPoint, endPoint: secondPoint, width: width, isLast: false)
         } else {
             let prePoint = strokePoints[count - 3].point
             let width = strokePoints[count - 2].width * lineWidth
-            addSidePoints(forPoint: midPoint, startPoint: prePoint, endPoint: lasPoint, width: width)
+            addSidePoints(forPoint: midPoint, startPoint: prePoint, endPoint: lasPoint, width: width, isLast: false)
         }
 
         if isStrokeFinished {
             let width = strokePoints[count - 1].width * lineWidth
-            addSidePoints(forPoint: lasPoint, startPoint: midPoint, endPoint: lasPoint, width: width)
+            addSidePoints(forPoint: lasPoint, startPoint: midPoint, endPoint: lasPoint, width: width, isLast: true)
+
         }
     }
 
-    private func addSidePoints(forPoint point: CGPoint, startPoint: CGPoint, endPoint: CGPoint, width: CGFloat) {
+    private func addSidePoints(forPoint point: CGPoint, startPoint: CGPoint, endPoint: CGPoint, width: CGFloat, isLast: Bool) {
         let points = StrokeUtils.sidePoints(forStartPoint: startPoint, midPoint: point,
                                             endPoint: endPoint, width: width)
         leftPoints.append(points.left)
         rightPoints.append(points.right)
+
+        guard leftPoints.count == rightPoints.count  else { return }
+
+        let count = leftPoints.count
+        if count == 1 {
+            lastLeftDrawnPoint = leftPoints[0]
+            lastRightDrawnPoint = rightPoints[0]
+        }
+
+
+
+        if count >= 2 {
+            let path = UIBezierPath()
+            let lastLeftPoint = leftPoints[count - 1]
+            let preLeftPoint = leftPoints[count - 2]
+            let leftMidPoint = (lastLeftPoint + preLeftPoint) / 2
+
+            let lastRightPoint = rightPoints[count - 1]
+            let preRightPoint = rightPoints[count - 2]
+            let rightMidPoint = (lastRightPoint + preRightPoint) / 2
+
+            path.moveToPoint(lastLeftDrawnPoint)
+            path.addQuadCurveToPoint(leftMidPoint, controlPoint: preLeftPoint)
+
+            if isLast {
+                path.addQuadCurveToPoint(lastLeftPoint, controlPoint: leftMidPoint)
+                path.addLineToPoint(lastRightPoint)
+                path.addQuadCurveToPoint(rightMidPoint, controlPoint: lastRightPoint)
+            } else {
+                path.addLineToPoint(rightMidPoint)
+            }
+
+            path.addQuadCurveToPoint(lastRightDrawnPoint, controlPoint: preRightPoint)
+            path.closePath()
+
+            fillPaths.append(path)
+
+            lastLeftDrawnPoint = leftMidPoint
+            lastRightDrawnPoint = rightMidPoint
+        }
     }
-    
+
 }
